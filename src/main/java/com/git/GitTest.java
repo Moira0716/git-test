20d85ea62aef8242b8d52586f85281193303ef9a
Yizhu <yizhu.pan@nyu.edu>
2022-02-05 22:45:01 -0500

    2nd commit

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -10,5 +10,6 @@ package com.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello, git!");
+        System.out.println("Hello, git!222222");
     }
 }
