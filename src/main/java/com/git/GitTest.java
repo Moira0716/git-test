7c8af25ace7e72c55389c22b68d4aa020e0b03e7
Yizhu <yizhu.pan@nyu.edu>
2022-02-05 22:35:02 -0500

    first commit

---
+++ src/main/java/com/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.git;
+
+/**
+ * @ClassName GitTest
+ * @Author yizhupan
+ * @Description
+ * @Date 2022/2/5 22:29
+ * @Version 1.0.0
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello, git!");
+    }
+}
